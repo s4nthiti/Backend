c67613aa53a75d02aaf6920622d850f19f47b9a8
s4nthiti <santhiti.4@mail.kmutt.ac.th>
2020-10-30 09:56:29 +0700

    commit for authentication complete now working for content controller

---
+++ ContentService.API/Controllers/ContentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using ContentService.API.Context;
+using ContentService.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentService.API.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ContentController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("posts")]
+        public async Task<Object> GetLastTenPosts()
+        {
+            var allPost = _context.Posts.Where(p => p.published == true).OrderBy(u => u.createDate).Take(10);
+            var sortPost = allPost.ToList();
+            return Ok(sortPost);
+        }
+
+
+
+    }
+}
---
+++ ContentService.API/Models/Contents/PostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentService.API.Models.Contents
+{
+    public class PostModel
+    {
+        public long Id { get; set; }
+        public int authorId { get; set; }
+        public string content { get; set; }
+        public string author { get; set; }
+        public DateTime createDate { get; set; }
+        public bool published { get; set; }
+    }
+}
